a12e0389cbfc435761a1b8012fe320da063d9c06
Dejan Prokić <dprokic@gmail.com>
2016-11-19 13:23:48 +0100

    First working scatterplot

--- src/main.js
+++ src/main.js
@@ -20,14 +20,46 @@ $(function () {
 // append the svg object to the body of the page
 // append a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
-    var svg = d3.select(".bar-chart").append("svg")
-        .attr("width", width + margin.left + margin.right + DEFAULTS.legend_width)
+    var svg = d3.select(".scatter-plot").append("svg")
+        .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom + DEFAULTS.brush_height)
         .append("g")
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
+    var x = d3.scaleLinear()
+        .range([0, width]);
 
-    // GO GO GO :)
+    var y = d3.scaleLinear()
+        .range([height, 0]);
 
-})
+    d3.tsv("/tcga-cases.tsv",
+        function(error, data) {
+            callbackError = error;
+            callbackData = data;
+            if (error) throw error;
+
+            // scale the range of the data
+            x.domain(d3.extent(data, function(d) { return d.case_age_at_diagnosis | 0; }));
+            y.domain([0, d3.max(data, function(d) { return d.case_days_to_death | 0; })]);
+
+            // Add the scatterplot
+            svg.selectAll("dot")
+                .data(data)
+                .enter().append("circle")
+                .attr("r", 5)
+                .attr("cx", function(d) { return x(d.case_age_at_diagnosis | 0); })
+                .attr("cy", function(d) { return y(d.case_days_to_death | 0); });
+
+            // Add the X Axis
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x));
+
+            // Add the Y Axis
+            svg.append("g")
+                .call(d3.axisLeft(y));
+
+        });
+
+});
