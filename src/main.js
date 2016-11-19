cb57a080969cbb80c5270185214f44d41df37405
mihajlo <mihajlo88@gmail.com>
2016-11-19 13:43:14 +0100

    jos y osa

--- src/main.js
+++ src/main.js
@@ -10,8 +10,8 @@ $(function() {
 
         graph_width: 800,
         graph_height: 500,
-        legend_width:100,
-        brush_height:50
+        legend_width: 100,
+        brush_height: 50
     };
 
     var margin = { top: 20, right: 20, bottom: 50, left: 60 },
@@ -35,7 +35,7 @@ $(function() {
         .range([0, width]).nice();
 
     var y = d3.scaleLinear()
-        .range([height, 0]).nice();
+        .range([ 0,height]).nice();
 
 
     var xCat = "DaysTD",
@@ -48,69 +48,73 @@ $(function() {
 
     d3.tsv("../tcga-cases.tsv", function(data) {
 
-      console.log(data);
+        console.log(data);
 
-    // case_age_at_diagnosis  case_days_to_death\
+        // case_age_at_diagnosis  case_days_to_death\
 
 
-      data.forEach(function(d) {
+        data.forEach(function(d) {
             d.ageAD = +d.case_age_at_diagnosis;
             d.daysTD = +d.case_days_to_death;
         });
 
         var xMax = d3.max(data, function(d) {
-                return d[xCatCol]; }) * 1.05,
+                return d[xCatCol];
+            }) * 1.05,
             xMin = d3.min(data, function(d) {
-                return d[xCatCol]; }),
+                return d[xCatCol];
+            }),
             xMin = xMin > 0 ? 0 : xMin,
             yMax = d3.max(data, function(d) {
-                return d[yCatCol]; }) * 1.05,
+                return d[yCatCol];
+            }) * 1.05,
             yMin = d3.min(data, function(d) {
-                return d[yCatCol]; }),
+                return d[yCatCol];
+            }),
             yMin = yMin > 0 ? 0 : yMin;
 
-        x.domain([xMin, xMax]);
+        x.domain([xMin, 10000]);
         y.domain([yMin, yMax]);
 
-       /* var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient("bottom")
-            .tickSize(-height);*/
+        /* var xAxis = d3.svg.axis()
+             .scale(x)
+             .orient("bottom")
+             .tickSize(-height);*/
 
-         /* d3.select(".axis")
-              .call(d3.axisBottom(x));*/
+        /* d3.select(".axis")
+             .call(d3.axisBottom(x));*/
 
         /*var yAxis = d3.svg.axis()
             .scale(y)
             .orient("left")
             .tickSize(-width);*/
 
-           /* d3.select(".axis")
-              .call(d3.axisLeft(y));*/
+        /* d3.select(".axis")
+           .call(d3.axisLeft(y));*/
 
-        var color =  d3.scaleOrdinal(d3.schemeCategory10);
+        var color = d3.scaleOrdinal(d3.schemeCategory10);
         //d3.schemeCategory10();
 
         var tip = d3.tip()
             .attr("class", "d3-tip")
             .offset([-10, 0])
             .html(function(d) {
-                return xCatCol + ": " + d[xCatCol] + "<br>" + yCatCol + ": " + d[yCatCol];
+                return xCat + ": " + d[xCatCol] + "<br>" + yCat + ": " + d[yCatCol];
             });
 
-        /*var zoomBeh = d3.behavior.zoom()
-            .x(x)
-            .y(y)
+       /* var zoomBeh = d3.zoom()
+            //.x(x)
+            //.y(y)
             .scaleExtent([0, 500])
-            .on("zoom", zoom);
+            .on("zoom", zoom);*/
 
-        var svg = d3.select("#scatter")
+        /*var svg = d3.select(".scatter-plot")
             .append("svg")
             .attr("width", outerWidth)
             .attr("height", outerHeight)
             .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-            .call(zoomBeh);*/
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            //.call(zoomBeh);*/
 
         svg.call(tip);
 
@@ -131,11 +135,11 @@ $(function() {
 
         svg.append("g")
             .classed("y axis", true)
-             .call(d3.axisLeft(x))
+            .call(d3.axisLeft(x))
             .append("text")
             .classed("label", true)
-            .attr("transform", "rotate(-90)")
-            .attr("y", -margin.left)
+            //.attr("transform", "rotate(-180deg)")
+            .attr("y", margin.left)
             .attr("dy", ".71em")
             .style("text-anchor", "end")
             .text(yCat);
@@ -165,10 +169,12 @@ $(function() {
             .enter().append("circle")
             .classed("dot", true)
             .attr("r", function(d) {
-                return 2 /*Math.sqrt(d[rCat] / Math.PI)*/; })
+                return 2 /*Math.sqrt(d[rCat] / Math.PI)*/ ;
+            })
             .attr("transform", transform)
             .style("fill", function(d) {
-                return color(d[colorCat]); })
+                return color(d[colorCat]);
+            })
             .on("mouseover", tip.show)
             .on("mouseout", tip.hide);
 
@@ -177,7 +183,8 @@ $(function() {
             .enter().append("g")
             .classed("legend", true)
             .attr("transform", function(d, i) {
-                return "translate(0," + i * 20 + ")"; });
+                return "translate(0," + i * 20 + ")";
+            });
 
         legend.append("circle")
             .attr("r", 3.5)
@@ -188,13 +195,40 @@ $(function() {
             .attr("x", width + 26)
             .attr("dy", ".35em")
             .text(function(d) {
-                return d; });
+                return d;
+            });
 
 
-         function transform(d) {
+        function transform(d) {
             return "translate(" + x(+d[xCatCol]) + "," + y(+d[yCatCol]) + ")";
-          }     
+        }
 
+
+        function zoom() {
+            svg.select(".x.axis").call(xAxis);
+            svg.select(".y.axis").call(yAxis);
+
+            svg.selectAll(".dot")
+                .attr("transform", transform);
+        }
+
+        d3.select("input").on("click", change);
+
+        function change() {
+            //xCat = "Carbs";
+            xMax = d3.max(data, function(d) {
+                return d[xCatCol]; });
+            xMin = d3.min(data, function(d) {
+                return d[xCatCol]; });
+
+            zoomBeh.x(x.domain([xMin, xMax])).y(y.domain([yMin, yMax]));
+
+            var svg = d3.select("#scatter").transition();
+
+            svg.select(".x.axis").duration(750).call(xAxis).select(".label").text(xCat);
+
+            objects.selectAll(".dot").transition().duration(1000).attr("transform", transform);
+        }
 
 
 
